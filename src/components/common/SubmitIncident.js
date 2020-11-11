bc6bd00f830628dfbc1db4f3f949d2cb33223fe0
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-11-11 14:55:58 -0700

    added prreliminary form components to SubmitIncident.js

--- src/components/common/SubmitIncident.js
+++ src/components/common/SubmitIncident.js
@@ -10,9 +10,34 @@ import {
 import React, { useState } from 'react';
 
 export default function SubmitIncident() {
+  const [userDetails, setUserDetails] = useState({
+    name: '',
+    date: '',
+    location: '',
+    category: '',
+    description: '',
+    email: '',
+  });
+
+  const handleChanges = e => {
+    e.persist();
+    setUserDetails({
+      ...userDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const formLayout = {};
+
   return (
     <div className="submission-form">
-      <Input placeholder="bazinga" />
+      <Form onValuesChange={handleChanges}>
+        <Input id="date" placeholder="date" />
+        <Input id="location" placeholder="location" />
+        <Select className="category" />
+        <Input.TextArea id="description" placeholder="description" />
+        <Button> SUBMIT </Button>
+      </Form>
     </div>
   );
 }
