5263621743e534672b77b81b744a818b29b420e9
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-11-11 16:31:41 -0500

    created basic layout for reducer

---
+++ src/state/reducers/submissionReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  name: '',
+  date: '',
+  category: '',
+  description: '',
+  email: '',
+  crossStreets: '',
+};
+
+export const submissionReducer = (state = initialState, action) => {
+  return state;
+};
