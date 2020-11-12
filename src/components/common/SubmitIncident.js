4709a68ea9e80ecd1424cca3c987578b5e79af95
Alex <schroedergoncalves@gmail.com>
2020-11-11 21:03:12 -0500

    Added form.item wrapper to each element in the incident submission form

--- src/components/common/SubmitIncident.js
+++ src/components/common/SubmitIncident.js
@@ -31,11 +31,28 @@ export default function SubmitIncident() {
 
   return (
     <div className="submission-form">
-      <Form onValuesChange={handleChanges}>
-        <Input id="date" placeholder="date" />
-        <Input id="location" placeholder="location" />
-        <Select className="category" />
-        <Input.TextArea id="description" placeholder="description" />
+      <Form onValuesChange={handleChanges} layout="vertical">
+        <Form.Item label="date">
+          <Input id="date" placeholder="date" />
+        </Form.Item>
+        <Form.Item label="location">
+          <Input id="location" placeholder="location" />
+        </Form.Item>
+        <Form.Item
+          label="category"
+          tooltip="The categories outlined here were taken from the National Institute of Justice' Use of Force Continuum. For more information, please visit: https://nij.ojp.gov/topics/articles/use-force-continuum "
+        >
+          <Select className="category" />
+        </Form.Item>
+        <Form.Item>
+          <Input.TextArea id="description" placeholder="description" />
+        </Form.Item>
+        <Form.Item>
+          <Input id="date" placeholder="date" />
+        </Form.Item>
+        <Form.Item>
+          <Input id="email" placeholder="jane.doe.com" />
+        </Form.Item>
         <Button> SUBMIT </Button>
       </Form>
     </div>
