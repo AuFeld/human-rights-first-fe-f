77ddf4d0f460369860a945cc2529ee6b0138add7
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-11-12 13:49:36 -0500

    Added ending month selector and included tags for each

--- src/components/common/GraphFilterForm.js
+++ src/components/common/GraphFilterForm.js
@@ -21,13 +21,30 @@ export default function GraphFilterForm() {
   ];
 
   return (
-    <div className="filter-box">
+    <div
+      className="filter-box"
+      style={{
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        padding: '2em',
+        width: '100%',
+      }}
+    >
       <div id="starting_month">
+        Pick a starting month:
         <Select style={{ width: 100 }}>
           {months.map(month => {
             return <Option value={month}>{month}</Option>;
           })}
         </Select>
+        <div id="ending_month">
+          Pick an ending month:
+          <Select style={{ width: 100 }}>
+            {months.map(month2 => {
+              return <Option value={month2}>{month2}</Option>;
+            })}
+          </Select>
+        </div>
       </div>
     </div>
   );
