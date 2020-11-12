b2c7d89e195e0f5844f07e97a11d0e01ed876e3a
Brandon Neil <63384257+bneil7@users.noreply.github.com>
2020-11-12 10:20:18 -0700

    implemented more styling elements with using AntDesign elements

--- src/components/common/SubmitIncident.js
+++ src/components/common/SubmitIncident.js
@@ -19,24 +19,75 @@ export default function SubmitIncident() {
     email: '',
   });
 
-  const handleChanges = e => {
-    e.persist();
-    setUserDetails({
-      ...userDetails,
-      [e.target.name]: e.target.value,
-    });
+  //   const handleChanges = e => {
+  //     e.persist();
+  //     setUserDetails({
+  //       ...userDetails,
+  //       [e.target.name]: e.target.value,
+  //     });
+  //   };
+
+  const formLayout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
   };
 
-  const formLayout = {};
+  const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+  };
 
   return (
     <div className="submission-form">
-      <Form onValuesChange={handleChanges}>
-        <Input id="date" placeholder="date" />
-        <Input id="location" placeholder="location" />
-        <Select className="category" />
-        <Input.TextArea id="description" placeholder="description" />
-        <Button> SUBMIT </Button>
+      <Form
+        // onValuesChange={handleChanges}
+        {...formLayout}
+        name="basic"
+      >
+        <Form.Item
+          label="Date"
+          name="date"
+          rules={[{ required: true, message: 'Please enter date!' }]}
+          style={{ width: 500 }}
+        >
+          <Input placeholder="MM/DD/YYYY" />
+        </Form.Item>
+
+        <Form.Item
+          label="Location"
+          name="location"
+          rules={[{ required: true, message: 'Please enter Location!' }]}
+          style={{ width: 500 }}
+        >
+          <Input placeholder="City, State" />
+        </Form.Item>
+
+        <Form.Item
+          label="Type of force used"
+          name="category"
+          rules={[{ required: true, message: 'Please choose at least one!' }]}
+          style={{ width: 500 }}
+        >
+          <Select className="category" placeholder="Select..." />
+        </Form.Item>
+
+        <Form.Item
+          label="Description of incident"
+          style={{
+            width: 500,
+          }}
+        >
+          <Input.TextArea
+            id="description"
+            placeholder="Please describe the details of the incident"
+          />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} style={{ width: 500 }}>
+          <Button type="primary" htmlType="submit">
+            {' '}
+            SUBMIT{' '}
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   );
